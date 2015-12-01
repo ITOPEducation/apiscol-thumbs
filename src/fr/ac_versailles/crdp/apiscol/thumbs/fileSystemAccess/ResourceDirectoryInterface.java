22ad82fc4c26356247111006bb5fa19395b0a01e
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2015-12-01 06:54:45 +0100

    Unuseful imports removal

--- src/fr/ac_versailles/crdp/apiscol/thumbs/fileSystemAccess/ResourceDirectoryInterface.java
+++ src/fr/ac_versailles/crdp/apiscol/thumbs/fileSystemAccess/ResourceDirectoryInterface.java
@@ -8,12 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.google.common.base.Function;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 
 import fr.ac_versailles.crdp.apiscol.utils.FileUtils;
@@ -31,6 +38,32 @@ public class ResourceDirectoryInterface {
 
 	public static void initialize(String path) {
 		fileRepoPath = path;
+		ignoreSSl();
+	}
+
+	private static void ignoreSSl() {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return new X509Certificate[0];
+			}
+
+			public void checkClientTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection
+					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (GeneralSecurityException e) {
+		}
 	}
 
 	public static String getFilePath(String thumbId) {
